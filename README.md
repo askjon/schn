892fd1d35dd7cc3a12f25ee45dc6df9c46974437
slybot <slybot@try.it>
2019-04-04 03:25:03 +0200

    1st Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# schn or the Secure Content Hosting Network 
+
+	This Project tries to connect the World's most refined Content Display Technologies (Webbrowsers) with the worlds "newest" development in distributed Networks.
+
+## Goals 
+
+	- Decentralizing of Content Hosting
+	- Democratizing public Forums
+	- Stabalizing Links
---
+++ src/main.c
@@ -0,0 +1,47 @@
+/*
+ *
+ * title: schn (Secure Content Hosting Network)
+ * type: peer2peer network
+ * auther: collin cole
+ * e-mail: askcollincole@gmail.com
+ *
+ */
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*for later implementations*/
+#include <pthread.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+void
+schn_loop()
+{
+int create
+
+
+}
+
+
+int
+main(int argc, char **argv)
+{
+	/* Initialization */
+
+	/* Main */
+
+	schn_loop();
+
+	/* Termination */
+
+
+	return 0;	
+}
