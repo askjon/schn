a952856dbc0de1c2c17a330bb8ba67bf476f83fa
slybot <slybot@try.it>
2019-04-04 04:22:11 +0200

    First working server

--- src/main.c
+++ src/main.c
@@ -21,12 +21,52 @@
 
 
 
+#define SCHN_RECV_BUFFER_SIZE 1024
 
 void
 schn_loop()
 {
-int create
+	int create_socket, new_socket;
+	socklen_t addrlen;
+	char *buffer = malloc(SCHN_RECV_BUFFER_SIZE * sizeof(char));
+	struct sockaddr_in address;
 
+	if((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
+	{
+		fprintf(stdout,"new Socket created\n");
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(1500);
+
+	if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0){    
+      fprintf(stdout,"Binding Socket\n");
+   }
+    
+    
+   while (1) {    
+      if (listen(create_socket, 10) < 0) {    
+         fprintf(stderr,"server: listen");    
+         exit(1);    
+      }    
+    
+      if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) < 0) {    
+         fprintf(stderr,"server: accept");    
+         exit(1);    
+      }    
+    
+      if (new_socket > 0){    
+         fprintf(stdout,"The Client is connected...\n");
+      }
+        
+      recv(new_socket, buffer, SCHN_RECV_BUFFER_SIZE, 0);    
+      fprintf(stdout,"%s\n", buffer);    
+      write(new_socket, "hello world\n", 12);    
+      close(new_socket);    
+   }    
+   close(create_socket);
+   return;
 
 }
 
